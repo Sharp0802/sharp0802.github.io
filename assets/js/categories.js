8e625efdaee9d7ea528cbb42670a746458ddb780
Sharp0802 <sharp0802.business@gmail.com>
2022-02-06 02:42:22 +0900

    Update files

--- assets/js/categories.js
+++ assets/js/categories.js
@@ -1,4 +1,7 @@
-const categories = { {% for category in site.categories %}{% capture category_name %}{{ category | first }}{% endcapture %}{{ category_name }}: [{% for post in site.categories[category_name] %}{ url: `{{ site.baseurl }}{{ post.url }}`, date: `{{post.date | date_to_string}}`, title: `{{post.title}}`},{% endfor %}],{% endfor %} }
+---
+---
+
+const categories = { {% for category in site.categories %}{% capture category_name %} { { category | first } } {% endcapture %} { { category_name } }: [{% for post in site.categories[category_name] %}{ url: `{{ site.baseurl }}{{ post.url }}`, date: `{{post.date | date_to_string}}`, title: `{{post.title}}` }, {% endfor %}], {% endfor %} }
 
 window.onload = function () {
   document.querySelectorAll(".category").forEach((category) => {
